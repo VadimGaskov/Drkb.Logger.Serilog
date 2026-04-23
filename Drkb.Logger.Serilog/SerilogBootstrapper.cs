131dce22e997392661e5acc1b2277e78def7dd1a
Vadim <gaskov.vadim@mail.ru>
2026-04-23 10:22:58 +0800

    init

---
+++ Drkb.Logger.Serilog/SerilogBootstrapper.cs
@@ -0,0 +1,13 @@
+﻿using Serilog;
+
+namespace Drkb.Logger.Serilog;
+
+public static class SerilogBootstrapper
+{
+    public static void CreateBootstrapLogger()
+    {
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console()
+            .CreateBootstrapLogger();
+    }
+}
---
+++ Drkb.Logger.Serilog/SerilogHostBuilderExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace Drkb.Logger.Serilog;
+
+public static class SerilogHostBuilderExtensions
+{
+    public static IHostBuilder UseDrkbSerilog(
+        this IHostBuilder hostBuilder,
+        IConfiguration configuration,
+        string applicationName)
+    {
+        return hostBuilder.UseSerilog((context, services, loggerConfiguration) =>
+        {
+            loggerConfiguration
+                .ReadFrom.Configuration(configuration)
+                .ReadFrom.Services(services)
+                .Enrich.FromLogContext()
+                .Enrich.WithProperty("Application", applicationName);
+        });
+    }
+}
